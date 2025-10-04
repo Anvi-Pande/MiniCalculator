466dd8a466f69ca9c835173bf03e7937c1f38fe2
Anvi-Pande <apande@go.pasadena.edu>
2025-10-03 17:23:03 -0700

    Add files via upload
    This is the code file that I am turning in for Iteration 1

---
+++ MiniCalculator-1.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+// Function prototypes
+void display(char op, double num1, double num2, double result);
+long long factorial(int n);
+double square(double n);
+double squareRoot(double n);
+
+//Main function
+int main()
+{
+    double num1, num2;
+    char op;
+    double result;
+    bool loop = true;
+    string inpt;
+
+    cout << "~~~~~~Mini Calculator~~~~~~" << endl;
+
+    while (loop)
+    {
+        cout << "Enter a number: ";
+        cin >> num1;
+        cout << "Enter an operator (Addition (+), Subtraction (-), Multiplication (*), Division (/), Power (^), Root (r), Factorial (!)): ";
+        cin >> op;
+        cout << "Enter another number: ";
+        cin >> num2;
+
+        if (op == '+')
+        {
+            result = num1 + num2;
+            display(op, num1, num2, result);
+        }
+        else if (op == '-')
+        {
+            result = num1 - num2;
+            display(op, num1, num2, result);
+        }
+        else if (op == '*')
+        {
+            result = num1 * num2;
+            display(op, num1, num2, result);
+        }
+        else if (op == '/')
+        {
+            if (num2 == 0)
+            {
+                cout << "Error: Division by zero!" << endl;
+            }
+            else
+            {
+                result = num1 / num2;
+                display(op, num1, num2, result);
+            }
+        }
+        else if (op == '^')
+        {
+            result = pow(num1, num2);
+            display(op, num1, num2, result);
+        }
+        else if (op == 'r')
+        {
+            if (num2 == 0)
+            {
+                cout << "Error: Root degree cannot be zero!" << endl;
+            }
+            else
+            {
+                result = pow(num1, 1.0 / num2);
+                display(op, num1, num2, result);
+            }
+        }
+        else if (op == '!')
+        {
+            result = factorial((int)num1);
+            if (result != -1)
+                cout << num1 << "! = " << result << endl;
+        }
+        else
+        {
+            cout << "Error: Invalid operator!" << endl;
+        }
+
+        cout << "Input (STOP) to close (enter anything else to continue): ";
+        cin >> inpt;
+        if (inpt == "STOP")
+        {
+            loop = false;
+        }
+    }
+}
+
+
+//Display function for main function operations
+void display(char op, double num1, double num2, double result)
+{
+    cout << "Equation: " << num1 << " " << op << " " << num2 << " = " << result << endl;
+}
+
+//Square function definition
+double square(double n) {
+    return n * n;
+}
+
+//Square root function definition
+double squareRoot(double n) {
+    if (n < 0) {
+        cout << "Error: Cannot take square root of negative number!" << endl;
+        return -1;
+    }
+    return sqrt(n);
+}
+
+//Factorial function definition
+long long factorial(int n)
+{
+    if (n < 0)
+    {
+        cout << "Error: Factorial of negative number is undefined!" << endl;
+        return -1;
+    }
+    long long fact = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        fact *= i;
+    }
+    return fact;
+}
